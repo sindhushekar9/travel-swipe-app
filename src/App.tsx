9f0b4b3d917139f8bd71525eee85ddd146f1a694
sindhushekar9 <sindhushekar.1995@gmail.com>
2026-02-21 11:51:08 -0500

    Setup basic structure within 'src' and added routing, material packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,9 +8,6 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
