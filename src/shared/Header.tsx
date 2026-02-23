9a34dec2f70a936965fc4c34b64c35d510fdb00f
sindhushekar9 <sindhushekar.1995@gmail.com>
2026-02-23 12:59:22 -0500

    Handle deployment issues

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,13 +1,24 @@
-import { AppBar, Avatar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Toolbar, Typography, Box } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <Avatar alt="Remy Sharp" src="/AppLogo.png" />
-        <Typography variant="h6">
-          GoWander
-        </Typography>
+        {/* Clickable logo + app name using Router Link */}
+        <Box
+          component={RouterLink}  // MUI Box renders as a Router Link
+          to="/"
+          display="flex"
+          alignItems="center"
+          gap={1}
+          sx={{ textDecoration: "none", color: "inherit" }}
+        >
+          <Avatar alt="GoWander Logo" src="/AppLogo.png" />
+          <Typography variant="h6" sx={{ userSelect: "none" }}>
+            GoWander
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
